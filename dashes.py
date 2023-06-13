1ab1010657664983fd13c01290ce3ed9b6227290
arjunkilaru <88005394+arjunkilaru@users.noreply.github.com>
2023-06-13 10:41:32 -0400

    Update dashes.py

--- dashes.py
+++ dashes.py
@@ -2,32 +2,6 @@ from dash import dcc, html, Dash, Input, Output, dash_table
 import pandas as pd
 import openpyxl
 
-def get_dates(date, dicf):
-    ret = pd.DataFrame()
-    class BreakOutOfLoops(Exception):
-        pass
-
-    for name in dicf.keys():
-        df = dicf[name]
-        dates = df['Dates'].tolist()
-        try:
-            for elem in dates:
-                for d in elem:
-                    if date in elem:
-                        row = df[df['Dates'] == elem].reset_index(drop = True)
-                        row['Unnamed: 0'] = name
-                        ret = pd.concat([ret, row])
-                        raise BreakOutOfLoops
-        except BreakOutOfLoops:
-            pass
-    ret = ret.sort_values(by = 'Average Volume Fraction', ascending = False)
-    ret = ret.rename(columns={ret.columns[0]: 'Ticker'})
-    return ret
-
-
-
-
-
 app = Dash(__name__)
 server = app.server
 xlsx_file = 'ticks.xlsx'
@@ -42,7 +16,10 @@ app.layout = html.Div([
         value=tickers[0]
     ),
     html.Div(id='table-container'),
-    dcc.Input(id='date-input', type='text', placeholder='Enter a date'),
+    html.Div([
+        dcc.Input(id='date-input', type='text', placeholder='Enter a date'),
+        html.Button('Submit', id='submit-button')
+    ]),
     html.Div(id='date-table-container'),
 ])
 
@@ -59,10 +36,11 @@ def display_table(selected_ticker):
 
 @app.callback(
     Output('date-table-container', 'children'),
-    Input('date-input', 'value')
+    Input('submit-button', 'n_clicks'),
+    State('date-input', 'value')
 )
-def display_date_table(date_input):
-    if date_input:
+def display_date_table(n_clicks, date_input):
+    if n_clicks and date_input:
         df = get_dates(date_input, xlsx_data)
         return dash_table.DataTable(
             data=df.to_dict('records'),
